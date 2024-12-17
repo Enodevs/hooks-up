29c3754da36f0d9b4524627f28dd1a241dbcc1a4
Enodevs <isiaqabdullah100@gmail.com>
2024-12-17 17:51:57 +0100

    Updated commits

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hi from npm")
+
+
+/**
+ * This is a react custom hook
+ * It returns a capitalize function that helps you capitalize the first letter of a word
+ * @example 
+ * import { useState } from 'react';
+ * import { useCapitalize } from 'hooks-up';
+ * 
+ * export default function Component({ children }) {
+ *     const [user, setUser] = useState("bobby")
+ *     const example = useCapitalize()
+ * 
+ *     return (
+ *         <div>
+ *           <p>{example(user)}<p/>
+ *           {children}   
+ *         </div>
+ *     )
+ * }
+ */
+
+export function useCapitalize() {
+    const capitalize = (str) => {
+        return String(str).charAt(0).toUpperCase() + String(str).slice(1)
+    }
+    return capitalize
+}
